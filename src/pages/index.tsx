14823459c1078de8a4af98a68989509b5e9024ab
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-03-04 19:44:45 +0100

    Use env vars

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,15 +30,15 @@ class Home extends Component<{}, {
     };
   }
 
-  componentDidMount(): void {
+  async componentDidMount(): Promise<void> {
     const httpLink = createHttpLink({
-      uri: 'https://api-euwest.graphcms.com/v1/ck73ixhlv09yt01dv2ga1bkbp/master',
+      uri: process.env.GRAPHQL_API_ENDPOINT,
     });
 
     const authLink = setContext((_, { headers }) => {
-      // get the authentication token from local storage if it exists
-      const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjoyLCJ0b2tlbklkIjoiMGEyZWYwZWYtYmY4Ny00ZmVlLTkzYmQtMzBkMzIzYzAwODM4IiwiaWF0IjoxNTgyNzQ2MDc1LCJpc3MiOiJodHRwczovL21hbmFnZW1lbnQuZ3JhcGhjbXMuY29tLyJ9.wE-IDwFnCNpwUj5AMeGsbf2CGcTgkT1KkE9_l6s-bU03sckdDgV2LXCHH5Fb8XwJGAkoeD5bK9yDnY6oNapiusvg4xRqY-hkRxgh8lB_lTXXpIka6HhjIJz1RQO_dObNGFqr41dKihseBqN5Ce4AJvQBIHyJasKX63xe2eEzdUnWfuNUmrG_XStRu1-xDKKp7vFVox272rr-LqgwRymF3eYcw7J-IAag4qpztoU6zNhKJNYzQKdMMejvMDNg34bHNp4TRIbIUIYpytkwCaAb5TsH98xEiFziU5rUr4EYToSltgE46VnqX2npm56qK-AGp5zauMZgvA20Djtb7BuYqGAqCdOHEQtjjyVLufCu6Y72i9gNQqFQ-WGQ6AFN84KT7BJgRoTruduYG9VhGMOR59HR3jG2QIWXOCt55aI9YwAGNQii0b_QqaoSO08Pb_Ooji5abFLISs70jQb-z1QcnvHIzHnsKqymEWwZhbkxpwf8bv8C6-8k4JGB5YdVj3T_0XQ-OCyvWQIGwVxKysLj8HBeVvXOKUyz7p-thOHbO4qSaRaV7w6_Yy2XtdwBlkIiTTqLezN34vCnsyhZ7N1IgLzp0bwNCyCoPOFs5Q9Ccw7hwJRP3kDT2cW4COJWVt-V5YF_9nnlZN8JjcIgv7FMZKoKRHi004vSosPYGd-v3Uw';
-      // return the headers to the context so httpLink can read them
+      const token = process.env.GRAPHQL_API_KEY || '';
+
+      // Return the headers to the context so httpLink can read them
       return {
         headers: {
           ...headers,
@@ -51,9 +51,12 @@ class Home extends Component<{}, {
       cache: new InMemoryCache(),
     });
 
-    buildGraphQLProvider({
+    const dataProvider = await buildGraphQLProvider({
       client,
-    }).then((dataProvider) => this.setState({ dataProvider }));
+    });
+    this.setState({
+      dataProvider,
+    });
   }
 
   render(): JSX.Element {
