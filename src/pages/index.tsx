5d319384f8824ff72b274f4145a72201db435304
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-03-05 06:46:39 +0100

    Attempt to use api-platform/admin, crashes with "Invariant failed: Hash history needs a DOM"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,81 +1,21 @@
-/** @jsx jsx */
-import { jsx } from '@emotion/core';
-import { createLogger } from '@unly/utils-simple-logger';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import ApolloClient from 'apollo-client';
-import { setContext } from 'apollo-link-context';
-import { createHttpLink } from 'apollo-link-http';
-import buildGraphQLProvider from 'ra-data-opencrud';
+import { HydraAdmin } from '@api-platform/admin';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
-import React, { Component } from 'react';
-import { Admin, Resource } from 'react-admin';
-import { ProductEdit } from '../components/admin/ProductEdit';
+import React from 'react';
 
-import { ProductList } from '../components/admin/ProductList';
 import Loader from '../components/Loader';
-import { GraphQLDataProvider } from '../types/GraphQLDataProvider';
+import { AppPageProps } from '../types/AppPageProps';
 
-const fileLabel = 'pages/index';
-const logger = createLogger({ // eslint-disable-line no-unused-vars,@typescript-eslint/no-unused-vars
-  label: fileLabel,
-});
+const Home = (props: AppPageProps): JSX.Element => {
+  const { isServer } = props;
 
-class Home extends Component<{}, {
-  dataProvider: GraphQLDataProvider;
-}> {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dataProvider: null,
-    };
+  if (!isServer) {
+    return <Loader />;
   }
 
-  async componentDidMount(): Promise<void> {
-    const httpLink = createHttpLink({
-      uri: process.env.GRAPHQL_API_ENDPOINT,
-    });
-
-    const authLink = setContext((_, { headers }) => {
-      const token = process.env.GRAPHQL_API_KEY || null;
-
-      // Return the headers to the context so httpLink can read them
-      return {
-        headers: {
-          ...headers,
-          authorization: token ? `Bearer ${token}` : null,
-        },
-      };
-    });
-    const client = new ApolloClient({
-      link: authLink.concat(httpLink),
-      cache: new InMemoryCache(),
-    });
-
-    const dataProvider = await buildGraphQLProvider({
-      client,
-    });
-    this.setState({
-      dataProvider,
-    });
-  }
-
-  render(): JSX.Element {
-    const { dataProvider } = this.state;
-
-    if (!dataProvider) {
-      return <Loader />;
-    }
-
-    return (
-      <Admin
-        title="Next Right Now - Admin"
-        dataProvider={dataProvider}
-      >
-        <Resource name="Product" list={ProductList} edit={ProductEdit} />
-      </Admin>
-    );
-  }
-}
+  return (
+    <HydraAdmin entrypoint={'https://demo.api-platform.com'} />
+    // <HydraAdmin entrypoint={process.env.GRAPHQL_API_ENDPOINT} />
+  );
+};
 
 export default Home;
