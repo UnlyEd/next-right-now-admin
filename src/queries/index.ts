ef28da956706d5c69c1073ceadd82d2f3b6e276b
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-03-06 19:43:51 +0100

    Getting tired

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -6,7 +6,7 @@ export default {
     [GET_LIST]: gql`
       fragment product on Product {
         id
-        title: description(locale: FR)
+        title: title(locale: FR)
 #        description: description(locale: FR)
 #        descriptionFR: description(locale: FR) XXX Using this makes the ra-data-opencru/getResponseParser crash when reading "field.type", because "field" doesn't match any known field and thus has no "type" prop
         price
