947b709f42083812147562e7d450dc370adff74f
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2020-03-08 21:48:55 +0100

    Tried to display customers choice input but crashes the view

--- src/components/admin/ProductCreate.tsx
+++ src/components/admin/ProductCreate.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Create, NumberInput, SimpleForm, TextInput } from 'react-admin';
+import { ReferenceInput, SelectInput, Create, NumberInput, SimpleForm, TextInput } from 'react-admin';
 
 const ProductCreate = (props): JSX.Element => {
   console.log('productCreate props', props);
   return (
     <Create {...props}>
       <SimpleForm>
+        {/*<ReferenceInput label="Customer" source="customer" reference="Customer">*/}
+        {/*  <SelectInput optionText="label" />*/}
+        {/*</ReferenceInput>*/}
         <TextInput source="titleEN" label={"Title (EN)"} />
         <TextInput source="titleFR" label={"Title (FR)"} />
-        <NumberInput source="price" />
+        <NumberInput source="price" defaultValue={5} />
       </SimpleForm>
     </Create>
   );
